a0ced0b823bd58974861b87c34495e68d9ea4c8f
nnsense91 <HD-74@yandex.ru>
2019-12-19 18:06:04 +0500

    Реализован клик для ввода результата и заготовка всплывающего окна для ввода.

--- frontend/src/admin/App.vue
+++ frontend/src/admin/App.vue
@@ -1,12 +1,18 @@
 <template lang="pug">
   .maincontent
+    enterResult(
+      v-if="resultIsClicked"
+      @matchResultOnOff="enterResultOnOff()"
+      :currentIndexX="currentIndexX"
+      :currentIndexY="currentIndexY"
+      )
     .championship
       table.championship__table
         thead.championship__head
           td.championship__head-cell.championship__head-team Команда
           td.championship__head-cell.championship__head-number №
           td.championship__head-cell(
-            v-for="(team, index) in ALLTEAMS"            
+            v-for="(team, index) in ALLTEAMS"
           ) {{ index+1 }}
         tbody.championship__table-body
           tr.championship__table-rows(
@@ -14,31 +20,45 @@
           )
             td {{ team.name }}
             td {{ indexX+1 }}
-            td.championship__table-results(              
+            td.championship__table-results(
               v-for="(team, indexY) in ALLTEAMS"
               )
-              matchResult(                
+              matchResult(
                 :indexX="indexX"
                 :indexY="indexY"
+                @matchResultOnOff="enterResultOnOff()"
+                @sendIndexes="changeCurrentIndexes(indexX+1, indexY+1)"
               )
-
-      
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
 export default {
   components: {
-    matchResult: () => import("./components/matchResult")
+    matchResult: () => import("./components/matchResult"),
+    enterResult: () => import("./components/enterResult")
   },
   data() {
-    return {};
+    return {
+      resultIsClicked: false,
+      currentIndexX: null,
+      currentIndexY: null,
+    };
   },
   mounted() {
     this.fetchTeams();
   },
   computed: mapGetters(["ALLTEAMS"]),
-  methods: mapActions(["fetchTeams"])
+  methods: {
+    enterResultOnOff: function() {
+      return this.resultIsClicked=!this.resultIsClicked;  
+    },
+    changeCurrentIndexes: function(x, y) {
+      this.currentIndexX = x;
+      this.currentIndexY = y; 
+    },
+    ...mapActions(["fetchTeams"])
+  }
 };
 </script>
 
